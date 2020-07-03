46ce7f25b961d4cf69cc544b8ea0d50b038242bf
Fabien Villepinte <fabien.villepinte@gmail.com>
2020-07-03 16:48:25 +0200

    Fix deprecation for symfony/config 4.2

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -24,8 +24,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('cl_slack');
+        $treeBuilder = new TreeBuilder('cl_slack');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('cl_slack');
+        }
 
         $rootNode
             ->children()
